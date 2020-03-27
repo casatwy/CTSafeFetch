aca489090f7438424c506a7c41228f5a5e899180
Casa Taloyum <casatwy@casatwy.com>
2020-03-27 11:29:00 +0800

    Initial Commit

---
+++ CTSafeFetch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CTSafeFetch
+//
+//  Created by casa on 2020/3/27.
+//  Copyright © 2020 casa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
